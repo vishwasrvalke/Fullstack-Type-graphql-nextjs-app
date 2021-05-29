e07340c1c5ab16800b240941f5470be7c6296b72
vishwasrvalke <39165365+vishwasrvalke@users.noreply.github.com>
2021-05-30 02:11:16 +0530

    updated mikroormsetup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config';
+
+const main = async() => {
+    const orm = await MikroORM.init(microConfig);
+    orm.getMigrator().up();
+    // const post = orm.em.create(Post,{title:'my first post'});
+    // await orm.em.persistAndFlush(post);
+
+    const posts = await orm.em.find(Post, {});
+    console.log(posts);
+};
+
+main();
