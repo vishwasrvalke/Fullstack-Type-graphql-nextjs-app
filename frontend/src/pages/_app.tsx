911e984f0aacb30ce74c433d0b22f3574f48eeab
vishwasrvalke <39165365+vishwasrvalke@users.noreply.github.com>
2021-06-06 12:13:36 +0530

    updated complete login and logout with caching and urql caching

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,87 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import theme from "../theme";
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+
+function newUpdateQuery<Result, Query>(
+  cache: Cache,
+  queryInput: QueryInput,
+  result: any,
+  func: (r: Result, q: Query) => Query
+) {
+  cache.updateQuery(queryInput, (data) => func(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          logout: (_result, args, cache, info) => {
+            newUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              () => ({ me: null })
+            );
+          },
+          login: (_result, args, cache, info) => {
+            newUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            newUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
